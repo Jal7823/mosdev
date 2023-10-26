71ed08d11ffee5d023a4fedfdee0bf4dd291b718
jal7823 <Jal7823@gmail.com>
2023-10-26 10:58:38 -0300

    :hammer: [test] test whitenoise

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,9 @@
 import os
-
+from django.conf import settings
+from whitenoise import WhiteNoise
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
