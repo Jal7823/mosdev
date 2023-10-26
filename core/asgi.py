9a9fd068632124770db080e868a3e91b09432e62
jal7823 <Jal7823@gmail.com>
2023-10-26 11:11:37 -0300

    :hammer: [test] delete whitenoises

--- core/asgi.py
+++ core/asgi.py
@@ -1,9 +1,9 @@
 import os
 from django.conf import settings
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+# application = WhiteNoise(application, root=settings.STATIC_ROOT)
