706a7ca7675ad5ccfeea07af79fc56551d8fe83b
jal7823 <jal7823@gmail.com>
2023-12-01 11:40:59 -0300

    :hammer: [fix] setting redoc as home

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
   # YOUR PATTERNS
   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
   path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-  path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+  path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
